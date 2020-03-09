9580000491d7f9f52e9106b45a400312b0131108
Tracey Flanders <7952207+traceyflanders@users.noreply.github.com>
2020-03-09 14:32:42 -0600

    Update default_credentials.h
    adjusted constants definitions

--- templates/ESP8266_and_Thingsboard_MQTT_PubSub_callback_template/default_credentials.h
+++ templates/ESP8266_and_Thingsboard_MQTT_PubSub_callback_template/default_credentials.h
@@ -1,19 +1,14 @@
-/*
- * Credentials header file
- */
-/////////////////////////////////////////////////////////////////////////////////
-/////////////////////// RENAME THIS FILE TO credentials.h ///////////////////////
-/////////////////////////////////////////////////////////////////////////////////
-
 /* 
  * WIFI Credentials 
  * Change to your wifi name and password 
  */
-#ifndef MY_WIFI_SSID 
+#ifndef MY_WIFI_SSID
   #define MY_WIFI_SSID "cool_wifi_name"
-  #define MY_WIFI_PASSWORD "super_duper_password"   
 #endif
 
+#ifndef  MY_WIFI_PASSWORD
+  #define MY_WIFI_PASSWORD "super_duper_password"  
+#endif
 
 /*
  * Thingsboard API Token 
@@ -23,11 +18,13 @@
   #define MY_TB_TOKEN "123456789"
 #endif
 
-
 /* MQTT credentials (Optional)
  * Change to your mqtt username & password
  */
 #ifndef MY_MQTT_USERNAME
-  #define MY_MQTT_USERNAME "abcefg"
-  #define MY_MQTT_PASSWORD "123456789"  
-#endif            
+  #define MY_MQTT_USERNAME "your_username_here"
+#endif
+
+#ifndef MY_MQTT_PASSWORD
+  #define MY_MQTT_PASSWORD "your_password_here"
+#endif
