cf320d7402ad330f2b66ce0752386b771030e09d
Tracey Flanders <7952207+traceyflanders@users.noreply.github.com>
2020-03-09 14:32:10 -0600

    Update default_credentials.h
    adjusted constants definitions

--- templates/ESP8266_and_Thingsboard_Template/default_credentials.h
+++ templates/ESP8266_and_Thingsboard_Template/default_credentials.h
@@ -9,7 +9,7 @@
  * WIFI Credentials 
  * Change to your wifi name and password 
  */
- #ifndef MY_WIFI_SSID
+#ifndef MY_WIFI_SSID
   #define MY_WIFI_SSID "cool_wifi_name"
 #endif
 
@@ -25,17 +25,14 @@
   #define MY_TB_TOKEN "123456789"
 #endif
 
-
 /* MQTT credentials (Optional)
  * Change to your mqtt username & password
  */
-
- #ifndef MY_MQTT_USERNAME
-  #define MY_MQTT_USERNAME "abcefg"
-   
+#ifndef MY_MQTT_USERNAME
+  #define MY_MQTT_USERNAME "your_username_here"
 #endif
 
 #ifndef MY_MQTT_PASSWORD
-  #define MY_MQTT_PASSWORD "123456789"
+  #define MY_MQTT_PASSWORD "your_password_here"
 #endif
       
